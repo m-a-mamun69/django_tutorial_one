3726a7715c9ccc43986378973889a3b1faf1702e
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-09-02 18:53:54 +0600

    Basic  Error.

---
+++ MyDjango/news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path("articles/<int:year>/", views.year_archive, name='year_archive'),
+# path("articles/<int:year>/<int:month>/", views.month_archive),
+# path("articles/<int:year>/<int:month>/<int:pk>/", views.article_detail),
+]
