b0a67a76614995ea0ad859ea43c0c6451a4d94d5
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-09-02 19:54:49 +0600

    Solved The Error 'Renderring'.

--- MyDjango/news/urls.py
+++ MyDjango/news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index', views.base, name='base'),
     path("articles/<int:year>/", views.year_archive, name='year_archive'),
 # path("articles/<int:year>/<int:month>/", views.month_archive),
 # path("articles/<int:year>/<int:month>/<int:pk>/", views.article_detail),
--- MyDjango/news/views.py
+++ MyDjango/news/views.py
@@ -3,6 +3,9 @@ from .models import Article
 
 # Create your views here.
 
+def base(request):
+    return render(request, "base.html")
+
 def home(request):
     return render(request, "home.html")
 
@@ -10,4 +13,4 @@ def home(request):
 def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
     context = {"year": year, "article_list": a_list}
-    return render(request, "news/year_archive.html", context)
+    return render(request, "year_archive.html", context)
